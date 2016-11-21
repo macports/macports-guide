macports/macports-guide

commit 265dc92b95edc3b12e25037724719e525188e46a

guide: escape string for regex replace
Special characters such as '&' could trigger strange behavior as they were
interpreted by the regex engine.

git-svn-id: https://svn.macports.org/repository/macports/trunk/doc-new@154113 d073be05-634f-4543-b044-5fe20cf6d1d6

toc-for-chunked.tcl
@@ -16,7 +16,7 @@ foreach path [glob -directory [lindex $argv 0] {*.html}] {
 
     set file [open $path r+]
     set data [read $file]
-    regsub {<body([^>]+)>} $data $replacement data
+    regsub {<body([^>]+)>} $data [regsub -all {\W} $replacement {\\&}] data
     regsub {</body>} $data {</div></body>} data
     seek $file 0
     puts $file $data

commit fc6576de63a75aaffc7db1eb42311898b8fe0240

Properly escape string for regex replace
Only escape special replacement syntax such as \0 or & in the
replacement string.

This is a follow-up to 265dc92b95edc3b12e25037724719e525188e46a which
broke the TOC output.

Closes: https://trac.macports.org/ticket/52924

toc-for-chunked.tcl
@@ -6,7 +6,7 @@
 set file [open "[lindex $argv 0]/index.html"]
 regexp {<div class="toc">.+?</div>} [read $file] replacement
 close $file
-set replacement "<body\\1><div class=\"book\">$replacement"
+set replacement "<body\\1><div class=\"book\">[regsub -all {&|\\[0-9]} $replacement {\\&}]"
 
 # Add the table of contents to all other html files.
 foreach path [glob -directory [lindex $argv 0] {*.html}] {
@@ -16,7 +16,7 @@ foreach path [glob -directory [lindex $argv 0] {*.html}] {
 
     set file [open $path r+]
     set data [read $file]
-    regsub {<body([^>]+)>} $data [regsub -all {\W} $replacement {\\&}] data
+    regsub {<body([^>]+)>} $data $replacement data
     regsub {</body>} $data {</div></body>} data
     seek $file 0
     puts $file $data